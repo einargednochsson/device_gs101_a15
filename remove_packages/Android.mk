7bcef125591e372f57068c6e383da5154cd7d9ce
Einar Gednochsson <pete80pro.pb@gmail.com>
2024-12-02 20:05:07 +0100

    gs101: remove packages

--- remove_packages/Android.mk
+++ remove_packages/Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePackagesDevice
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := Flash
+LOCAL_OVERRIDES_PACKAGES := Flash CalendarGoogle Drive Gmail2 Maps Photos FilesGoogle talkback YouTube
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
